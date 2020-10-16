92f376a5be563dc27d9ed0167f57dd3a75e85c47
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-16 22:01:31 +0100

    update ui with cart functionalities

--- Shopping-Cart/app.js
+++ Shopping-Cart/app.js
@@ -83,6 +83,7 @@ class UI {
         // display cart item
         this.addCartItem(cartItem);
         // show the cart
+        this.showCart();
       });
     });
   }
@@ -117,7 +118,24 @@ class UI {
       `;
 
     cartContent.appendChild(div);
-    console.log(cartContent);
+  }
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
+  }
+  hideCart() {
+    cartOverlay.classList.remove("transparentBcg");
+    cartDOM.classList.remove("showCart");
+  }
+  setupAPP() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener("click", this.showCart);
+    closeCartBtn.addEventListener("click", this.hideCart);
+  }
+  populateCart(cart) {
+    cart.forEach((item) => this.addCartItem(item));
   }
 }
 // local storage
@@ -134,12 +152,20 @@ class Storage {
   static saveCart() {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+
+  static getCart() {
+    return localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+  // setup appp
 
+  ui.setupAPP();
   // get all products
   products
     .getProducts()
