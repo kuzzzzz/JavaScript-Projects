3409f064fd1a27f5a1922da3839cc133e8010154
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-16 21:04:39 +0100

    save,set and display product in cart

--- Shopping-Cart/app.js
+++ Shopping-Cart/app.js
@@ -73,14 +73,51 @@ class UI {
         event.target.disabled = true;
         // get product from products
         let cartItem = { ...Storage.getProducts(id), amount: 1 };
-        console.log(cartItem);
+
         // add product to the cart
+        cart = [...cart, cartItem];
         // save cart in local storage
+        Storage.saveCart(cart);
         // set cart values
+        this.setCartValues(cart);
         // display cart item
+        this.addCartItem(cartItem);
         // show the cart
       });
     });
+  }
+
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map((item) => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+            <img src=${item.image} alt="product">
+
+                    <div>
+                        <h4>${item.title}</h4>
+                        <h5>${item.price} </h5>
+                        <span class="remove-item" data-id=${item.id}>remove</span>
+                    </div>
+                    <div>
+                        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                        <p class="item-amount">${item.amount}</p>
+                        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+                    </div>
+      `;
+
+    cartContent.appendChild(div);
+    console.log(cartContent);
   }
 }
 // local storage
@@ -92,6 +129,10 @@ class Storage {
   static getProducts(id) {
     let products = JSON.parse(localStorage.getItem("products"));
     return products.find((product) => product.id === id);
+  }
+
+  static saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
