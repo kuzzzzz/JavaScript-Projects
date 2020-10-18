735d7d9e8267c5c33a03a96baea429fa1ae5aca6
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-18 21:58:17 +0100

    loaded over https

--- Movie-info/main.js
+++ Movie-info/main.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
 });
 function getMovies(searchText) {
   axios
-    .get("http://www.omdbapi.com/?apikey=f0c8c30c&s=" + searchText)
+    .get("https://www.omdbapi.com/?apikey=f0c8c30c&s=" + searchText)
     .then((response) => {
       let movies = response.data.Search;
       let output = "";
@@ -46,7 +46,7 @@ function getMovie() {
   let movieId = sessionStorage.getItem("movieId");
 
   axios
-    .get("http://www.omdbapi.com?apikey=f0c8c30c&i=" + movieId)
+    .get("https://www.omdbapi.com?apikey=f0c8c30c&i=" + movieId)
     .then((response) => {
       console.log(response);
       let movie = response.data;
@@ -73,7 +73,7 @@ function getMovie() {
             <h3>Plot</h3>
             ${movie.Plot}
             <hr>
-            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
             <a href="index.html" class="btn btn-default">Go Back To Search</a>
           </div>
         </div>
