41a3dd8c7c1d5b2d02057cbb5b179b41c1501e5a
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-20 15:55:49 +0100

    Initial mark up and styling

---
+++ quiz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "What is nancy's favourite food",
+    a: "chicken and chips",
+    b: "eba and eguisi soup",
+    c: "starch and banga soup",
+    d: "fried rice and chicken",
+    correct: "a",
+  },
+  {
+    question: "Who is the vice president of the US",
+    a: "Bill gates",
+    b: "Will smith",
+    c: "Taylor swift",
+    d: "Mike pence",
+    correct: "d",
+  },
+  {
+    question: "What is the most popular programming language in 2019",
+    a: "HTML",
+    b: "JavaScript",
+    c: "Python",
+    d: "Java",
+    correct: "b",
+  },
+  {
+    question: "What is the meaning of HTML",
+    a: "High Time My Lover",
+    b: "Heroplane Train Marbach Lambohgini",
+    c: "HyperText Mark Up Language",
+    d: "HyperText Make Up Language",
+    correct: "a",
+  },
+  {
+    question: "What is the meaning of SARs",
+    a: "Special Arm robery Squad",
+    b: "Special Army robery Squad",
+    c: "Special Anti rober Square",
+    d: "Special Anti robery Squard",
+    correct: "d",
+  },
+];
