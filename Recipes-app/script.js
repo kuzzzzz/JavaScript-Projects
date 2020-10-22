0dc14774bdfc1cb7a9159f90b65c2f5972ac7ed4
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-22 21:16:01 +0100

    displayed products from api

--- Recipes-app/script.js
+++ Recipes-app/script.js
@@ -1,3 +1,60 @@
-document.querySelector(".button").addEventListener("click", () => {
-  document.querySelector(".red").classList.toggle("active");
-});
+// document.querySelector(".button").addEventListener("click", () => {
+//   document.querySelector(".red").classList.toggle("active");
+// });
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal() {
+  const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+  const respData = await res.json();
+  const randomMeal = respData.meals[0];
+
+  addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+  const meal = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+}
+
+async function getMealsBySearch(term) {
+  const meals = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+}
+
+function addMeal(mealData, random = false) {
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
+
+  meal.innerHTML = `
+                <div class="meal-header">
+                   
+                     ${
+                       random
+                         ? `
+                    <span class="random">
+                        Random Recipe
+                    </span>
+                   `
+                         : ""
+                     }
+                   
+                    <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
+                </div>
+                <div class="meal-body">
+                    <h4>${mealData.strMeal}</h4>
+                    <button class="button">
+                        <svg class="red" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                        </svg>
+                    </button>
+                </div>
+  `;
+
+  meals.appendChild(meal);
+}
