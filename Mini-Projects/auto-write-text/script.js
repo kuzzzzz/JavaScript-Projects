36dfded98c7c6f5b93337d65ec71ada7faf4bd99
Atumah Daniel <atumahdaniel@gmail.com>
2020-11-24 12:15:49 +0100

    auto write script for up to three diff text

---
+++ Mini-Projects/auto-write-text/script.js
@@ -0,0 +1,46 @@
+const text = "Hey, are you digging the green text on black?";
+const text2 = "I know you are? cus I am 😉";
+const text3 = "Oo and I love ❤️️ my gf Nancy, to the  🌕 and 🔙 .";
+
+let index = 0;
+function writeText(params) {
+  document.body.innerText = params.slice(0, index);
+  index++;
+  if (index > params.length) {
+    index = 0;
+  }
+}
+
+setTimeout(() => {
+  var stop = setInterval(() => {
+    writeText(text);
+  }, 100);
+  setTimeout(() => {
+    clearInterval(stop);
+    document.body.innerText = text;
+  }, 4000);
+}, 1000);
+
+setTimeout(() => {
+  var stop2 = setInterval(() => {
+    writeText(text2);
+  }, 100);
+  setTimeout(() => {
+    clearInterval(stop2);
+    document.body.innerText = text2;
+  }, 3000);
+}, 7000);
+
+setTimeout(() => {
+  var stop3 = setInterval(() => {
+    writeText(text3);
+  }, 100);
+  setTimeout(() => {
+    clearInterval(stop3);
+    document.body.innerHTML = `${text3}  <span class='rel'>⟲</span>`;
+    let x = document.querySelector(".rel");
+    x.addEventListener("click", () => {
+      location.reload();
+    });
+  }, 4500);
+}, 12500);
