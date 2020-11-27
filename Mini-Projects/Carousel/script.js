cd64e61a8c20961e9aa5b2fc75d047ab6d14a9a9
Atumah Daniel <atumahdaniel@gmail.com>
2020-11-27 15:17:44 +0100

    Carousel using set timeout

--- Mini-Projects/Carousel/script.js
+++ Mini-Projects/Carousel/script.js
@@ -5,13 +5,14 @@ const img = document.querySelectorAll("#imgs img");
 let idx = 0;
 
 function run() {
-    idx++;
+  idx++;
+  if (idx > img.length - 1) {
+    idx = 0;
+  }
 
-    if (idx > img.length - 1) {
-        idx = 0;
-    }
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
 
-    imgs.style.transform = `translateX(${-idx * 500}px)`;
+  setTimeout(run, 2000);
 }
 
-setInterval(run, 2000);
+run();
