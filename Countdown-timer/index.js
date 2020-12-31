2652de22473e3d2440f309470cc887d282142a2e
Atumah Daniel <atumahdaniel@gmail.com>
2020-12-31 22:05:14 +0100

    New message

--- Countdown-timer/index.js
+++ Countdown-timer/index.js
@@ -2,6 +2,8 @@ const daysEL = document.getElementById("days");
 const hoursEL = document.getElementById("hours");
 const minutesEL = document.getElementById("minutes");
 const secondsEL = document.getElementById("seconds");
+const container = document.getElementById("container");
+const heading = document.getElementById("heading");
 
 const newYears = "1 Jan 2021";
 
@@ -28,5 +30,13 @@ function formatTime(time) {
   return time < 10 ? `0${time}` : time;
 }
 
-countdown();
-setInterval(countdown, 1000);
+var x = new Date();
+
+if (x.getFullYear() == 2021) {
+  container.innerHTML = "";
+  heading.innerText = "";
+  document.body.style = "background-image:url(happy-new-year.gif)";
+} else {
+  countdown();
+  setInterval(countdown, 1000);
+}
